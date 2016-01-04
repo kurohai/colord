41d0d815c82cfe0814c58ab03e96898aa972c881
kurohai <kurohai@gmail.com>
2016-01-04 07:24:31 -0600

    initial commit

---
+++ flaskapp/settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: kurohai
+# @Date:   2016-01-04 07:17:36
+# @Last Modified by:   root
+# @Last Modified time: 2016-01-04 07:20:29
+
+appname = 'Color Divination'
+appnamed = 'colord'
+SECRET_KEY = 'ReallBigPassphraseWithRandomStringenydM2ANhdcoKwdVa0jWvEsbPFuQpMjf'
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Kurohai's Flask Template
+
+
+## TODO:
+- migrate stuff to settings.py
+- add logging using log model
