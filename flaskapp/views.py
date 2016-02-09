33f48f2d4272d9f6960564a70389b33b96df42de
kurohai <kurohai@gmail.com>
2016-02-09 15:18:33 -0600

    added newline split, removed old routes

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -10,8 +10,8 @@ from dateme import parse_date
 
 blueprint = Blueprint(flasktemplate.appnamed, __name__)
 
-@blueprint.route('/colors/add2/', methods=['GET', 'POST'])
-def color_add2():
+@blueprint.route('/colors/add/', methods=['GET', 'POST'])
+def color_add():
     if request.method == 'GET':
         log.info('GET /color/add/')
     elif request.method == 'POST':
@@ -26,6 +26,14 @@ def color_add2():
         parsed_date = input_date.strftime('%Y-%m-%d')
         log.info('parsed date: {0}'.format(parsed_date))
         day = session.query(ColorSet).filter(ColorSet.date == parsed_date).first()
+
+        tmp = list()
+        for c in data.colors:
+            n = c.split('\r\n')
+            for nerp in n:
+                tmp.append(nerp)
+        data.colors = tmp
+
 
         # get the correct colorset
         log.info('setting colorset...')
@@ -44,6 +52,7 @@ def color_add2():
         # add colors to colorset
         for c in data.colors:
             c = c.strip()
+            pprint(c)
             log.info(c)
             color = Color()
             color.name = c
@@ -67,77 +76,6 @@ def color_add2():
 
     form = ColorForm(csrf_enabled=False)
     return render_template('public/color-form.html', text='', form=form)
-
-
-
-@blueprint.route('/colors/add-old/', methods=['GET', 'POST'])
-def color_add_old():
-    if request.method == 'GET':
-        log.info('GET /color/add/')
-    elif request.method == 'POST':
-        log.info('POST /colors/add')
-        today = session.query(ColorSet).filter(ColorSet.date == datetime.datetime.today().strftime('%Y-%m-%d')).first()
-        if today:
-            if today.id:
-                log.info('set found')
-                colorset = today
-            else:
-                log.info('new set created')
-                colorset = ColorSet()
-                colorset.date = datetime.datetime.today()
-        else:
-            log.info('new set created')
-            today = colorset = ColorSet()
-
-        data = dicto(request.form)
-
-        data.colors = data.colors[0].split(',')
-        colors = session.query(Color).all()
-        color_names = [c.name for c in colors]
-        for d in data.colors:
-            d = d.strip().lower()
-            color = Color()
-            color.colorset = colorset
-            if d not in color_names:
-                log.info('adding color: {0}'.format(d))
-                color.name = d
-                session.add(color)
-                session.commit()
-            else:
-                old_color = session.query(Color).filter(Color.name == d).first()
-                log.info('old color id: {0}, old color name: {1}'.format(old_color.id, old_color.name))
-                # print old_color.id, old_color.name
-                if old_color is not None:
-                    colorset.colors.append(old_color)
-
-                    # colorset.date = datetime.datetime.today() + datetime.timedelta(days=1)
-                    # print 'not adding color:', d
-                    log.info('not adding color: {0}'.format(d))
-
-                session.add(colorset)
-                session.commit()
-
-
-
-
-
-    form = ColorForm(csrf_enabled=False)
-    return render_template('public/color-form.html', text='', form=form)
-
-
-@blueprint.route('/colors/add/', methods=['GET', 'POST'])
-def color_add():
-    form = ColorForm(csrf_enabled=False)
-    if request.method == 'GET':
-        log.info('GET /color/add/')
-    elif request.method == 'POST':
-        log.info('POST /colors/add')
-    return render_template('public/color-form.html', text='', form=form)
-
-
-
-
-
 
 
 
